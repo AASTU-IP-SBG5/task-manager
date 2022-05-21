0511412af5a67b7ea25fc1ac613528eeacd5b3f6
kaliktg <86680782+kaliktg@users.noreply.github.com>
2022-05-21 21:35:09 +0000

    refined user home page

--- templates/user/signup.php
+++ templates/user/signup.php
@@ -137,13 +137,12 @@
                 </div>
             </div>
             <!--  k- added an upload picture form input -->
-            <!-- <div class="form-group">
-                <label class="control-label col-xs-4">Upload profile picture</label>
-                <div class="col-xs-8">
-                    <input class="form-control" autocomplete="new-password" id="id_password2" name="password2"
-                        required="" type="file" />
+            <div class="form-group">
+                <div class="upload">
+                    <label class="control-label col-xs-4">Upload profile picture</label>
+                    <input type="file" autocomplete="new-password"  name="profileUpload"/>
                 </div>
-            </div> -->
+            </div>
             <div class="form-group">
                 <div class="col-xs-8 col-xs-offset-4">
                     <p><label class="checkbox-inline"><input type="checkbox" required="required"> I accept the <a
