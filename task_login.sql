d951c97d07567b1d95dc4ca42b1ca827abd1b8c5
kaliktg <86680782+kaliktg@users.noreply.github.com>
2022-06-07 11:31:54 +0000

    updated task_login.sql file

--- task_login.sql
+++ task_login.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 21, 2022 at 09:32 PM
+-- Generation Time: Jun 07, 2022 at 01:30 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.12
 
@@ -24,18 +24,48 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `adminstrator`
+--
+
+CREATE TABLE `adminstrator` (
+  `id` int(10) NOT NULL,
+  `admin_name` varchar(100) NOT NULL,
+  `admin_password` varchar(100) NOT NULL,
+  `admin_position` text NOT NULL,
+  `admin_status` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `adminstrator`
+--
+
+INSERT INTO `adminstrator` (`id`, `admin_name`, `admin_password`, `admin_position`, `admin_status`) VALUES
+(1, 'kaleab', '12345', 'manager', 1);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `project`
 --
 
 CREATE TABLE `project` (
   `project_id` int(100) NOT NULL,
   `project_name` varchar(150) NOT NULL,
-  `description` mediumtext NOT NULL,
-  `user_id` int(10) NOT NULL,
+  `pro_description` mediumtext NOT NULL,
   `updated` date NOT NULL,
   `created` date NOT NULL,
-  `task_count` int(100) NOT NULL
+  `task_count` int(100) NOT NULL,
+  `deadline` date NOT NULL,
+  `user_id` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `project`
+--
+
+INSERT INTO `project` (`project_id`, `project_name`, `pro_description`, `updated`, `created`, `task_count`, `deadline`, `user_id`) VALUES
+(1, 'kal', 'bdffg', '2022-06-07', '2022-06-07', 0, '2022-06-18', 3),
+(2, 'programming', 'programming course study plan for the next semester', '2022-06-07', '2022-06-07', 0, '2022-06-25', 1);
 
 -- --------------------------------------------------------
 
@@ -58,17 +88,27 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`id`, `username`, `email`, `user_type`, `password`) VALUES
 (1, 'kal', 'kiogir@gmail.com', 'user', '202cb962ac59075b964b07152d234b70'),
 (2, 'hhh', 'admin@admin.com', 'admin', '202cb962ac59075b964b07152d234b70'),
-(3, 'sdasd', 'admin@admin.com', 'user', '202cb962ac59075b964b07152d234b70');
+(3, 'sdasd', 'admin@admin.com', 'user', '202cb962ac59075b964b07152d234b70'),
+(4, 'sdasd', 'kiogir00@gmail.com', 'user', '202cb962ac59075b964b07152d234b70'),
+(5, 'sdasd', 'kiogir00@gmail.com', 'user', '202cb962ac59075b964b07152d234b70'),
+(6, 'kaleabg', 'admin@admin.com', 'user', '827ccb0eea8a706c4c34a16891f84e7b');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `adminstrator`
+--
+ALTER TABLE `adminstrator`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `project`
 --
 ALTER TABLE `project`
-  ADD PRIMARY KEY (`project_id`);
+  ADD PRIMARY KEY (`project_id`),
+  ADD KEY `project_ibfk_1` (`user_id`);
 
 --
 -- Indexes for table `user`
@@ -81,16 +121,32 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `adminstrator`
+--
+ALTER TABLE `adminstrator`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `project`
 --
 ALTER TABLE `project`
-  MODIFY `project_id` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `project_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `project`
+--
+ALTER TABLE `project`
+  ADD CONSTRAINT `project_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
